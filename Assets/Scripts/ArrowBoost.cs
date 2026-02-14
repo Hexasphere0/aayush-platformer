a7bf2b143e468f2cdb90551b67256b40a63441a9
Aayush <aayush15genius@gmail.com>
2026-02-14 13:37:26 -0800

    Fixed Arrow Boost

--- Assets/Scripts/ArrowBoost.cs
+++ Assets/Scripts/ArrowBoost.cs
@@ -1,9 +1,11 @@
+using System.Collections;
 using UnityEngine;
 
 public class ArrowBoost : MonoBehaviour
 {
     [Header("Boost Velocity")]
     public float boostVelocity;
+    public float boostDuration;
 
     // Private Variables
     new Rigidbody2D rigidbody;
@@ -25,7 +27,32 @@ public class ArrowBoost : MonoBehaviour
             return;
         }
 
+        
+
         Vector2 boostDirection = Mathf.Cos(transform.eulerAngles.z * Mathf.Deg2Rad) * Vector2.right + Mathf.Sin(transform.eulerAngles.z * Mathf.Deg2Rad) * Vector2.up;
-        rigidbody.AddForce(boostDirection * boostVelocity, ForceMode2D.Impulse);
+
+        Vector2 parallelComponent = Vector2.Dot(boostDirection, rigidbody.linearVelocity) * boostDirection;
+        rigidbody.AddForce(-(rigidbody.linearVelocity - parallelComponent), ForceMode2D.Impulse);
+
+        if(Vector2.Dot(boostDirection, parallelComponent) < 0){
+            rigidbody.AddForce(-parallelComponent, ForceMode2D.Impulse);
+        }
+
+        Vector2 impulse = boostDirection * boostVelocity;
+
+        rigidbody.AddForce(impulse, ForceMode2D.Impulse);
+
+        StartCoroutine(player.FreezeMovement(boostDuration));
+        StartCoroutine(player.FreezeFriction(boostDuration));
+        StartCoroutine(player.FreezeGravity(boostDuration));
+
+        StartCoroutine(endBoost(impulse));
+    }
+
+    IEnumerator endBoost(Vector2 impulse)
+    {
+        yield return new WaitForSeconds(boostDuration);
+
+        rigidbody.AddForce(new Vector2(0, -impulse.y/2f), ForceMode2D.Impulse);
     }
 }
