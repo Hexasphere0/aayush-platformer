a73ad3a7240a90c641d03dc82427c1c33af721c0
Hexasphere0 <kesavgurugubelli@gmail.com>
2026-02-17 19:51:49 -0800

    boss fight work

--- Assets/Scripts/BossControllers/BossController2.cs
+++ Assets/Scripts/BossControllers/BossController2.cs
@@ -13,6 +13,16 @@ public class BossController2 : MonoBehaviour
     [Header("Color Change Attack Settings")]
     public float ticksBetweenColorChange;
     public int colorChangeCount;
+
+    [Header("Checkpoint Settings")]
+    public float ticksBetweenCheckpointSpawns;
+    public float checkpointSpawnCount;
+    public float timeToCollectCheckpoints;
+    public Vector2 checkpointSpawnOffset;
+    public GameObject bossCheckpointPrefab;
+    public Transform canvas;
+
+    [Header("Misc")]
     
     public GameObject levelTileParent;
     List<GameObject> levelTiles;
@@ -43,8 +53,20 @@ public class BossController2 : MonoBehaviour
                 for(int i = 0; i < colorChangeCount; i++)
                 {
                     int objectIndex = UnityEngine.Random.Range(0, levelTiles.Count);
-                    Debug.Log(objectIndex);
-                    levelTiles[objectIndex].GetComponent<LayerChanger>().ChangeLayerNonWhite();
+                    levelTiles[objectIndex].GetComponent<TransformingBlock>().StartTransforming();
+                }
+            }
+
+            // Spawn a checkpoint for you to collect
+            if(ticks % ticksBetweenCheckpointSpawns == 0)
+            {
+                for(int i = 0; i < checkpointSpawnCount; i++)
+                {
+                    int objectIndex = UnityEngine.Random.Range(0, levelTiles.Count);
+                    Vector2 checkpointPosition = (Vector2) levelTiles[objectIndex].transform.position + Vector2.up * (levelTiles[objectIndex].transform.localScale / 2) + checkpointSpawnOffset;
+
+                    GameObject checkpoint = Instantiate(bossCheckpointPrefab, checkpointPosition, Quaternion.identity, canvas);
+                    checkpoint.GetComponent<BossFightCheckpoint>().Initialize(timeToCollectCheckpoints);
                 }
             }
         }
---
+++ Assets/Scripts/BossFightCheckpoint.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BossFightCheckpoint : MonoBehaviour
+{
+    float initialTime;
+    float timeRemaining = 0;
+    bool active = false;
+
+    public Image timerRadial;
+
+    void Update()
+    {
+        timeRemaining -= Time.deltaTime;
+        timerRadial.fillAmount = timeRemaining / initialTime;
+
+        if(timeRemaining <= 0 && active)
+        {
+            BossFightManager.LooseBossFight();
+            Destroy(gameObject);
+        }
+    }
+
+    public void Initialize(float time)
+    {
+        initialTime = time;
+        timeRemaining = time;
+
+        active = true;
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(active && collision.gameObject.name == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
